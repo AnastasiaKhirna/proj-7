6430948c708748cd9f0108c113b3136858b88df8
AnastasiaKhirna <pupil25_siv@school52.kiev.ua>
2025-05-10 22:47:27 +0300

    адапти на пк

---
+++ index.js
@@ -0,0 +1,8 @@
+// const elcard = document.querySelector(".wrap_card");
+// console.log(elcard);
+
+// elcard.addEventListener("click", () => {
+//     elcard.classList.toggle("active");
+// });
+
+
