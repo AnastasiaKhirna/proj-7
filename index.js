778506bd50d0a247ae4eb2da1ef4119f4460a389
AnastasiaKhirna <pupil25_siv@school52.kiev.ua>
2025-05-18 22:22:20 +0300

    footer

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-// const elcard = document.querySelector(".wrap_card");
-// console.log(elcard);
-
-// elcard.addEventListener("click", () => {
-//     elcard.classList.toggle("active");
-// });
-
 
