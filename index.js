b3cb8c44ff41d06bcc0da8ac2308255dec140b30
AnastasiaKhirna <pupil25_siv@school52.kiev.ua>
2025-05-20 21:55:57 +0300

    ,

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const list = document.querySelector(".container_photos");
+const wraper = document.querySelectorAll(".wrap_up-text");
 
+list.addEventListener("click", (event) => {
+    const currentElem = event.target.closest(".wrap_card");
+    const prevElem = list.querySelector(".active");
+
+    if (prevElem) {
+        prevElem.classList.remove("active");
+    }
+
+    if (currentElem === prevElem) {
+        currentElem.classList.remove("active");
+    } else {
+        const elem = event.target.closest(".wrap_card");
+        elem.classList.toggle("active");
+    }
+});
